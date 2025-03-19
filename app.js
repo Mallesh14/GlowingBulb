d374cb6b54e8e66512632cb82e83d5414c348b61
MALLESWARAN.P <161807499+Mallesh14@users.noreply.github.com>
2025-03-19 22:38:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+function bulb_on()
+{
+    img.src="https://cdn.pixabay.com/photo/2014/04/03/10/32/bulb-310821_1280.png"
+
+}
+function bulb_off()
+{
+    img.src="https://cdn.pixabay.com/photo/2012/04/10/17/34/lightbult-26606_1280.png"
+}
